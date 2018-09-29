407d5b33d08086ac0f17f602bd7c599752417e38
Utku Aysev <utkua557@gmail.com>
2018-09-29 17:50:51 +0300

    Fixed App.java problems

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -19,50 +19,57 @@ import spark.template.mustache.MustacheTemplateEngine;
 public class App {
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
-
         get("/", (req, res) -> "Hello, World");
 
         post("/compute", (req, res) -> {
-            System.out.println(req.queryParams("input1"));
-            System.out.println(req.queryParams("input2"));
+            String result="";
+            try {
 
-            String input1 = req.queryParams("input1");
-            java.util.Scanner sc1 = new java.util.Scanner(input1);
-            sc1.useDelimiter("[;\r\n]+");
-            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-            while (sc1.hasNext()) {
-                int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
-                inputList.add(value);
+                System.out.println(req.queryParams("input1"));
+                System.out.println(req.queryParams("input2"));
+
+                String input1 = req.queryParams("input1");
+                java.util.Scanner sc1 = new java.util.Scanner(input1);
+                sc1.useDelimiter("[;\r\n]+");
+                java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+                while (sc1.hasNext()) {
+                    int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
+                    inputList.add(value);
+                }
+                System.out.println(inputList);
+
+                String input2 = req.queryParams("input2");
+                java.util.Scanner sc2 = new java.util.Scanner(input2);
+                sc2.useDelimiter("[;\r\n]+");
+                String[] inputArraystr;
+                String arrayString = "";
+                while (sc2.hasNext()) {
+                    int value2 = Integer.parseInt(sc2.next().replaceAll("\\s", ""));
+                    arrayString += value2 + ",";
+                }
+                inputArraystr = arrayString.split(",");
+                Integer[] inputArray = new Integer[inputArraystr.length];
+                for (int i = 0; i < inputArraystr.length; i++) {
+                    inputArray[i] = Integer.parseInt(inputArraystr[i]);
+                }
+                for (int i = 0; i < inputArray.length; i++) {
+                    System.out.println(inputArray[i] + ",");
+                }
+
+                String input3 = req.queryParams("input3").replaceAll("\\s", "");
+                boolean divide = Boolean.parseBoolean(input3);
+
+                String operation = req.queryParams("input4").replaceAll("\\s", "");
+
+                 result = App.method(inputList, inputArray, divide, operation);
+
+                System.out.println(result);
             }
-            System.out.println(inputList);
-
-            String input2 = req.queryParams("input2");
-            java.util.Scanner sc2 = new java.util.Scanner(input2);
-            sc2.useDelimiter("[;\r\n]+");
-            String[] inputArraystr;
-            String arrayString="";
-            while (sc1.hasNext()) {
-                int value2 = Integer.parseInt(sc2.next().replaceAll("\\s", ""));
-                arrayString+=value2+",";
+            catch (Exception e){
+                 result="There is an unexpected failure.Check the parameters";
             }
-            inputArraystr=arrayString.split(",");
-            Integer[] inputArray = new Integer[inputArraystr.length];
-            for (int i = 0; i < inputArraystr.length; i++) {
-                inputArray[i]= Integer.parseInt(inputArraystr[i]);
-            }
-            for (int i = 0; i < inputArray.length; i++) {
-                System.out.println(inputArray[i]+",");
-            }
-
-            String input3 = req.queryParams("input3").replaceAll("\\s","");
-            boolean divide = Boolean.parseBoolean(input3);
-
-            String operation = req.queryParams("input4").replaceAll("\\s","");
-
-            String result = App.method(inputList, inputArray,divide,operation);
-
             Map map = new HashMap();
-           map.put("result", result);
+            map.put("result", result);
             return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
@@ -85,11 +92,10 @@ public class App {
     }
 
 
-
     public static String method(ArrayList<Integer> arrayList, Integer[] array, boolean divide, String operation) {
         String output = "";
-        if (arrayList == null || array == null) {
-            output = "Arrays can not be null.";
+        if (arrayList == null || array == null || array.length == 0 || arrayList.size() == 0) {
+            output = "Arrays can not be null or empty.";
         } else {
             ArrayList<Integer> newArrayList;
             Integer[] newArray;
